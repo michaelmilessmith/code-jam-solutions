10f27277943585b6e3c9cebc43d0ae040606ae3e
michaelmilessmith <michaelmilessmith@gmail.com>
2017-02-15 11:32:13 +0000

    Adds solution to cookie-clicker-alpha

--- JavaScript/Code-Jam-2014/Qualification Round/cookie-clicker-alpha.js
+++ JavaScript/Code-Jam-2014/Qualification Round/cookie-clicker-alpha.js
@@ -9,6 +9,47 @@ const read = (fileName) => {
 var dataSmall = read("B-small-practice.in");
 var dataLarge = read("B-Large-practice.in");
 
-exports.timeToTarget = (currentCookiesPerSecond, target) => {
-  return target / currentCookiesPerSecond;
+exports.calculateTimeTaken = (cookiesPerSecond, costOfFarm, cookiesPerFarmPerSecond, cookieTarget) => {
+  let baseTime = Infinity,
+      newTime = cookieTarget / cookiesPerSecond
+    , timeToBuyFarms = 0;
+  for (let i = 1; baseTime > newTime; i++){
+    baseTime = newTime;
+    timeToBuyFarms += costOfFarm / (cookiesPerSecond + ((i - 1) * cookiesPerFarmPerSecond));
+    newTime = timeToBuyFarms + (cookieTarget / (cookiesPerSecond + (i * cookiesPerFarmPerSecond)));
+    // console.log({i, baseTime, newTime, timeToBuyFarms});
+  }
+  return baseTime;
+}
+
+
+exports.solveProblem = (cookiesPerSecond, costOfFarm, cookiesPerFarmPerSecond, cookieTarget) => {
+    return this.calculateTimeTaken(cookiesPerSecond, costOfFarm, cookiesPerFarmPerSecond, cookieTarget);
 };
+
+exports.solve = (data) => {
+  const cases = data[0],
+        length = data.length,
+        resultLines = [];
+  for (let i = 1; i < length; i++){
+    // console.log(`Case #${i}`);
+    const [c, f, x] = data[i].split(" ");
+    const result = this.solveProblem(2.0, c, f, x);
+    resultLines.push(`Case #${i}: ${Math.round( result * 10000000 ) / 10000000}`)
+  }
+  return resultLines
+};
+
+fs.writeFile("B-small.out", this.solve(dataSmall).join("\n"), (err) => {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+});
+
+fs.writeFile("B-large.out", this.solve(dataLarge).join("\n"), (err) => {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+});
--- JavaScript/Code-Jam-2014/Qualification Round/cookie-clicker-alpha.test.js
+++ JavaScript/Code-Jam-2014/Qualification Round/cookie-clicker-alpha.test.js
@@ -4,11 +4,20 @@ const expect = require("chai").expect;
 const cookieClickerAlpha = require("./cookie-clicker-alpha");
 
 describe("Cookie Clicker Alpha", () => {
-  it("should return how long to reach a target number of cookies", () => {
-    let result = cookieClickerAlpha.timeToTarget(2, 500);
-    expect(Math.round( result * 10000000 ) / 10000000).to.equal(250);
+  it("should match example tests", () => {
+    let result = cookieClickerAlpha.solveProblem(2.0, 30.0, 1.0, 2.0);
+    expect(Math.round( result * 10000000 ) / 10000000).to.equal(1.0000000);
 
-    result = cookieClickerAlpha.timeToTarget(6, 500);
-    expect(Math.round( result * 10000000 ) / 10000000).to.equal(83.3333333);
+    result = cookieClickerAlpha.solveProblem(2.0, 30.0, 2.0, 100.0);
+    expect(Math.round( result * 10000000 ) / 10000000).to.equal(39.1666667);
+
+    result = cookieClickerAlpha.solveProblem(2.0, 30.50000, 3.14159, 1999.19990);
+    expect(Math.round( result * 10000000 ) / 10000000).to.equal(63.9680013);
+
+    result = cookieClickerAlpha.solveProblem(2.0, 500.0, 4.0, 2000.0);
+    expect(Math.round( result * 10000000 ) / 10000000).to.equal(526.1904762);
+
+    result = cookieClickerAlpha.solveProblem(2.0, 1.03397, 99.87614, 99999.03277);
+    expect(Math.round( result * 10000000 ) / 10000000).to.equal(0.6518189);    
   });
 });
